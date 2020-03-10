8165fb0b9477a81e78a7d5352ac46386797e0e48
suzairi <selma.uzairi@htl-salzburg.ac.at>
2020-03-10 16:05:10 +0100

    Initial Commit

---
+++ Assets/Scripts/TextAppear.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAppear : MonoBehaviour {
+
+ [SerializeField]
+private Text winText; // Insert your text object inside unity inspector
+ 
+void Start()
+{
+    winText.enabled = false; // You may need to use .SetActive(false);
+}
+ 
+ 
+void OnCollisionEnter(Collision collision)
+{
+   if(collision.gameObject.tag == "BlueBall")
+{
+// This is where you make your text object appear on screen
+    winText.enabled = true; // May need to use .SetActive(true);
+}
+}
+ 
+void OnCollisionExit(Collision collision)
+{
+     // Here is where you make the text disappear off screen
+      if(collision.gameObject.tag == "BlueBall")
+{
+   winText.enabled = false; // May need to use .SetActive(false);
+}
+}
+}
